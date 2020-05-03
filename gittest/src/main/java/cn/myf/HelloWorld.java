27f5e8a0f6725dcb11e5171e3453706e524bcb99
Doris <603175593@qq.com>
2020-05-03 11:47:13 +0800

    idea的第一次修改提交到本地库

--- gittest/src/main/java/cn/myf/HelloWorld.java
+++ gittest/src/main/java/cn/myf/HelloWorld.java
@@ -4,4 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
     }
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
