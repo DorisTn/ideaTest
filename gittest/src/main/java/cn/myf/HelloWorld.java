5a8684534663bc9f1a1c4177f125a5504815a778
Doris <603175593@qq.com>
2020-05-03 12:11:03 +0800

    修改firstclassgogoo

--- gittest/src/main/java/cn/myf/HelloWorld.java
+++ gittest/src/main/java/cn/myf/HelloWorld.java
@@ -3,8 +3,9 @@ package cn.myf;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        test();
     }
-    public void test(){
+    private static void test(){
         int i=0;
         i+=10;
         System.out.println(i);
